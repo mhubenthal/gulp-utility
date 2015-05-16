05a24142c395459f5b3ba992102460509bf5a3f6
mhubenthal <hubentmx@gmail.com>
2015-05-15 21:31:35 -0400

    remove js filename

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var gulp = require('gulp'),
   plugins = gulpLoadPlugins();
 
 gulp.task('js',function(){
-  gulp.src('test.js')
+  gulp.src('')
 		.pipe(plugins.jshint())
 		.pipe(plugins.jshint.reporter('default'))
 });
 
 gulp.task('watch',function(){
-	gulp.watch(['test.js'], function(event){
+	gulp.watch([''], function(event){
 		gulp.run('js');
 	});	
 })
